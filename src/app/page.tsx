4bc62057e0c30bc34e40e94ad936d03259ae5ff2
HiChon <59905927+chon26909@users.noreply.github.com>
2023-07-13 17:29:44 +0700

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,30 +15,28 @@ import React from "react";
 
 const page = async () => {
   const posts = await getGalleryList({ limit: 5, offset: 1 });
-
   return (
     <main className="mt-5">
       <div className="text-2xl font-bold text-center">Gallery</div>
       <div>
-        {posts &&
-          posts.photos.map((item: any, index: number) => {
-            return (
-              <Link href={`/${item.id}`}>
+        {posts?.photos.map((item, index) => {
+          return (
+            <Link href={`/${item.id}`}>
+              <div>
+                <Image
+                  src={item.url}
+                  alt={item.title}
+                  width={300}
+                  height={300}
+                />
                 <div>
-                  <Image
-                    src={item.url}
-                    alt={item.title}
-                    width={300}
-                    height={300}
-                  />
-                  <div>
-                    {index + 1} {item.title}
-                  </div>
-                  <div>{item.description}</div>
+                  {index + 1} {item.title}
                 </div>
-              </Link>
-            );
-          })}
+                <div>{item.description}</div>
+              </div>
+            </Link>
+          );
+        })}
       </div>
     </main>
   );
