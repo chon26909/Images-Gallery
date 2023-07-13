d61f11c3613b3f2e882cafde4e0518c4ee5d3f02
HiChon <59905927+chon26909@users.noreply.github.com>
2023-07-13 21:28:26 +0700

    style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,27 +14,36 @@ import React from "react";
 // };
 
 const page = async () => {
-  const posts = await getGalleryList({ limit: 5, offset: 1 });
+  const posts = await getGalleryList({ limit: 52, offset: 0 });
   return (
-    <main className="mt-5">
-      <div className="text-2xl font-bold text-center">Gallery</div>
-      <div>
+    <main className="mt-5 container mx-auto">
+      <div className="my-5 text-3xl font-bold text-center">Photo Gallery</div>
+      <div className="grid grid-cols-fluid">
         {posts?.photos.map((item, index) => {
           return (
-            <Link href={`/${item.id}`}>
-              <div>
-                <Image
-                  src={item.url}
-                  alt={item.title}
-                  width={300}
-                  height={300}
-                />
+            <div
+              key={index}
+              className="m-5 border rounded-md overflow-hidden transition duration-150 hover:scale-110"
+            >
+              <Link href={`/${item.id}`}>
                 <div>
-                  {index + 1} {item.title}
+                  <Image
+                    className="w-full"
+                    src={item.url}
+                    alt={item.title}
+                    width={0}
+                    height={0}
+                    sizes="100"
+                  />
+                  <div className="p-4">
+                    <div className="text-lg font-semibold truncate">
+                      {item.title}
+                    </div>
+                    <div className="text-sm truncate">{item.description}</div>
+                  </div>
                 </div>
-                <div>{item.description}</div>
-              </div>
-            </Link>
+              </Link>
+            </div>
           );
         })}
       </div>
