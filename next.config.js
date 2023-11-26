ac447ca94d2a09c5918e3ebd3b0491edd524b8c5
HiChon <59905927+chon26909@users.noreply.github.com>
2023-11-26 22:35:27 +0700

    output: 'export'

--- next.config.js
+++ next.config.js
@@ -1,15 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-          {
-            protocol: 'https',
-            hostname: 'api.slingacademy.com',
-            port: '',
-            pathname: '/public/sample-photos/**',
-          },
-        ],
+  output: "export",
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "api.slingacademy.com",
+        port: "",
+        pathname: "/public/sample-photos/**",
       },
-}
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
